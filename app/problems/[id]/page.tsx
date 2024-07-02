6038ac481c7a6925b6ff89201ad7c85f590ce09d
Aditya Gudadhe <adigudadhe579@gmail.com>
2024-07-02 23:15:41 +0530

    Added persistence

--- app/problems/[id]/page.tsx
+++ app/problems/[id]/page.tsx
@@ -3,16 +3,26 @@
 import { useParams } from "next/navigation"
 import { Excalidraw } from "@excalidraw/excalidraw"
 import ExcalidrawWrapper from "@/components/excalidraw-wrapper"
+import { ExcalidrawElement } from "@excalidraw/excalidraw/types/element/types"
+import { AppState } from "@excalidraw/excalidraw/types/types"
 
 type paramType = {
   id: string
 }
 
+interface totalPayloadtype{
+  elements: readonly ExcalidrawElement[] | null,
+}
+
 export default function Id(){
   const params = useParams<paramType>()
-  const id = Number(params.id);
   // document.title = String(id);
+  let initialPayload: totalPayloadtype|null = null;
+  const recievedPayload:string|null = localStorage.getItem(params.id);
+  if (recievedPayload) {
+    initialPayload = JSON.parse(recievedPayload);
+  }
   return <div>
-    <ExcalidrawWrapper/>
+    <ExcalidrawWrapper id={params.id} initialPayload={initialPayload}/>
   </div>
 }
--- components/excalidraw-wrapper.tsx
+++ components/excalidraw-wrapper.tsx
@@ -1,14 +1,32 @@
 "use client";
 import { Excalidraw, convertToExcalidrawElements } from "@excalidraw/excalidraw";
+import React, { ReactElement, useEffect, useState } from "react"
+import { ExcalidrawElement } from "@excalidraw/excalidraw/types/element/types"
+import { AppState } from "@excalidraw/excalidraw/types/types"
 
-// import "@excalidraw/excalidraw/index.css";
-import React from "react"
+interface totalPayloadtype{
+  elements: readonly ExcalidrawElement[] | null,
+}
 
-const ExcalidrawWrapper: React.FC = () => {
+export default function ExcalidrawWrapper({ id, initialPayload } : {id:string, initialPayload: totalPayloadtype}):ReactElement{
+  const [exElements, setexElements] = useState<readonly ExcalidrawElement[] | null>(null);
+  useEffect(() => {
+    setTimeout(()=>{
+      const totalPayload:totalPayloadtype = {
+        elements: exElements,
+      }
+      localStorage.setItem(id, JSON.stringify(totalPayload));
+    },2000);
+  }, [exElements])
+
   return (
-    <div style={{height:"500px", width:"1000px"}}>
-      <Excalidraw />
+    <div className="h-screen w-screen">
+      <Excalidraw initialData={{
+        elements: initialPayload.elements,
+      }
+      } onChange={(excalidrawElements, appState, files)=>{
+        setexElements(excalidrawElements);
+      }} />
     </div>
   );
 };
-export default ExcalidrawWrapper;
